ba26aad4ee4228dd1a1a83cd33db039e8f21f9e2
Giovanni García <giovannigarcia.de@hotmail.com>
2020-11-12 12:28:58 -0500

    Arquitectura inicial para SoundBets Catalog

---
+++ src/SoundBeats.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SoundBeats.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
